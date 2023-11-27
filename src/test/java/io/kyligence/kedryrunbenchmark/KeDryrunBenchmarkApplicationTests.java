0314a7b7e302e7a9107f5831b10983dce0d303b4
zhong.zhu <mike950523@icloud.com>
2023-11-27 18:52:29 +0800

    KE-42482 project init

---
+++ src/test/java/io/kyligence/kedryrunbenchmark/KeDryrunBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kyligence.kedryrunbenchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeDryrunBenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
