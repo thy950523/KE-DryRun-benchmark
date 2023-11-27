12e2abe3cd3ac1638ef09c849cff3dce372b0ba9
zhong.zhu <mike950523@icloud.com>
2023-11-27 19:27:44 +0800

    KE-42482 project init

--- src/main/java/io/kyligence/benchmark/Application.java
+++ src/main/java/io/kyligence/benchmark/Application.java
@@ -1,13 +1,17 @@
 package io.kyligence.benchmark;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		// ! Benchmark Started
+		log.info("Start to run benchmark");
 	}
 
 }
